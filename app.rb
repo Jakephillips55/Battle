add66461222c49b16752d08aef34ea439b4907dc
Jakephillips55 <Jakep629@gmail.com>
2020-01-10 12:17:29 +0000

    functional mulit clicker

--- app.rb
+++ app.rb
@@ -27,13 +27,13 @@ class Battle < Sinatra::Base
     erb :attack
   end
   #
-  # get '/confirm_attack' do
+   # get '/confirm_attack' do
   #  @player_1_name = $player_1.name
   #  @player_2_name = $player_2.name
   #   @player_1_hp = $player_1.dmg
   #   @player_2_hp = $player_2.dmg
-  #   erb :confirm_attack
-  # end
+   #   erb :confirm_attack
+   # end
 
   run! if app_file ==$0
 end
