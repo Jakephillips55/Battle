abb9e83a3e4681010ffe20aaacf3f01f9d8b7897
Jakephillips55 <Jakep629@gmail.com>
2020-01-10 12:22:37 +0000

    fixing tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -5,7 +5,7 @@ describe Battle, type: :feature do
   feature "Enter names" do
     scenario 'players fill in names' do
       sign_in_and_play
-      expect(page).to have_content("Alicia vs. Shaun")
+      expect(page).to have_content("Alicia vs Shaun")
     end
   end
 end
